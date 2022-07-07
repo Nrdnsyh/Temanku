c95e8de0236f50041356848f454345678e5379c9
Nrdnsyh <88859005+Nrdnsyh@users.noreply.github.com>
2022-07-07 17:34:17 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-TOKEN = os.environ.get("TOKEN", None) # Dapatkan di @botfather
-OWNER = os.environ.get("OWNER", "kenkanasw") # Isi dengan username kalian tanpa tanda @
-GROUP = os.environ.get("GROUP", "musikkugroup") # Isi dengan username group kalian tanpa tanda @ kalau gak punya gak usah isi
-CHANNEL = os.environ.get("CHANNEL", "musikkuchannel") # Isi dengan username channel kalian tanpa tanda @ kalau gak punya gak usah isi
+TOKEN = os.environ.get("5422747960:AAGjkINHNz7xNiN_PRzGyRagz8j_zlnq4Js", None) # Dapatkan di @botfather
+OWNER = os.environ.get("OWNER", "AndiNrdnsyh") # Isi dengan username kalian tanpa tanda @
+GROUP = os.environ.get("GROUP", "AndiNrdnsyh") # Isi dengan username group kalian tanpa tanda @ kalau gak punya gak usah isi
+CHANNEL = os.environ.get("CHANNEL", "AndiNrdnsyh") # Isi dengan username channel kalian tanpa tanda @ kalau gak punya gak usah isi

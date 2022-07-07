c601b0264952ad4e353b15a03888391a9451e72b
Nrdnsyh <88859005+Nrdnsyh@users.noreply.github.com>
2022-07-07 17:26:32 +0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+TOKEN = os.environ.get("TOKEN", None) # Dapatkan di @botfather
+OWNER = os.environ.get("OWNER", "kenkanasw") # Isi dengan username kalian tanpa tanda @
+GROUP = os.environ.get("GROUP", "musikkugroup") # Isi dengan username group kalian tanpa tanda @ kalau gak punya gak usah isi
+CHANNEL = os.environ.get("CHANNEL", "musikkuchannel") # Isi dengan username channel kalian tanpa tanda @ kalau gak punya gak usah isi
